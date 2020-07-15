e8cb9f79043b2b21405cef4ee7f7b989eb6521aa
Drephyx Yango <drephyx@bewave.io>
2020-07-15 15:56:38 -0400

    card-service

---
+++ server/src/models/cards.model.js
@@ -0,0 +1,26 @@
+// cards-model.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const modelName = 'cards';
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+  const schema = new Schema({
+    name: { type: String, required: true },
+    listId: { type: Schema.Types.ObjectId, ref: 'lists' },
+  }, {
+    timestamps: true
+  });
+
+  // add unique constraint of combination of name and boardId
+  schema.index({ 'name': 1, 'listId': 1}, { 'unique': true });
+
+  // This is necessary to avoid model compilation errors in watch mode
+  // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
+  if (mongooseClient.modelNames().includes(modelName)) {
+    mongooseClient.deleteModel(modelName);
+  }
+  return mongooseClient.model(modelName, schema);
+
+};
